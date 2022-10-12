04f006ed8673a365ca85fdd5be29de9e7866e58b
smitha_ml <smitha.ml@tadigital.com>
2022-10-12 21:20:19 +0530

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import {Card, CardImg,  CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import {Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
  //Comment component begins
 class CommentForm extends Component{
@@ -99,7 +100,7 @@ class CommentForm extends Component{
         return (
             <div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -3,6 +3,12 @@ import * as ActionTypes from './ActionTypes';
 
 export const Comments = (state = COMMENTS, action)=>{
     switch(action.type){
+        case ActionTypes.ADD_COMMENTS:
+            return {...state, errMess: null, comments: action.payload};
+
+        case ActionTypes.COMMENTS_FAILED:
+            return {...state, errMess: action.payload};
+
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
             comment.id = state.length;
